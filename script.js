5b00cbe51f446ef7a62bb718db787865df106269
salismazaya <salismazaya@gmail.com>
2023-05-25 18:21:59 +0800

    commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const cubism2Model = "RAM/RAM.model3.json";
+(async function main() {
+    const app = new PIXI.Application({
+        view: document.getElementById("canvas"),
+        autoStart: true,
+        resizeTo: window
+    });
+
+    const model = await PIXI.live2d.Live2DModel.from(cubism2Model);
+
+    app.stage.addChild(model);
+
+    model.scale.set(0.25);
+    model.x = -100
+    model.y = 20
+    let interval;
+
+    channel.bind('play', function (data) {
+        let open = true;
+        interval = setInterval(() => {
+            model.internalModel.coreModel.setParameterValueById('ParamMouthOpenY', open ? 1 : 0)
+            open = !open
+        }, 200)
+    });
+
+    channel.bind('stop', (data) => {
+        clearInterval(interval)
+    })
+
+})();
